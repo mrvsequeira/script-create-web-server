134db042760fdd57ef8cc57efcd8995e9edd0e74
mrvsequeira <109689698+mrvsequeira@users.noreply.github.com>
2022-08-27 07:03:41 -0300

    Create script-apache-server.sh

---
+++ script-apache-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Atualizando servidor Ubuntu:"
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando o Apache2"
+
+apt install apache2 -y
+
+echo "Instalando o Unzip"
+
+apt install unzip -y
+
+echo "Baixando para a o diretório /tmp a aplicação"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Extraindo a aplicação na pasta /var/www/html."
+
+unzip main.zip
+cd linux-site-dio-main/
+cp -r * /var/www/html/
